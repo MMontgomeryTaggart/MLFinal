a247ca1c7371be64f1ad1bcfe7f88f4a3b750a0f
MMontgomeryTaggart <shah@IM-CAR314.local>
2017-12-15 21:03:04 -0700

    Obtained results for bagged forest and logistic regression.

--- Models/TestNotNNModels.py
+++ Models/TestNotNNModels.py
@@ -1,7 +1,10 @@
 from NotNN.BaggedForest import BaggedForest
 from NotNN.SVM import SVM
+from NotNN.LogisticRegression import LogisticRegression
+from NotNN.NaiveBayes import NaiveBayes
 from sklearn.model_selection import StratifiedKFold
 import numpy as np
+from sklearn.metrics import precision_recall_fscore_support
 import pickle
 from scipy.sparse import csr_matrix
 
@@ -17,75 +20,18 @@ y_train = data["labels"]
 x_test = csr_matrix(testData["features"])
 y_test = testData["labels"]
 
-# Test SVM ******************************
-svmC = [3000, 1000, 100]
-rate = [.5, .1, .01, .001, .00001]
-numEpochs = 40
-bestParams = {"params": {}, "accuracy" : 0}
-configCount = 1
-numConfigs = len(svmC) * len(rate)
-for C in svmC:
-    for r in rate:
-        print "Evaluating configuration %i of %i. (%.2f%%)" % (configCount, numConfigs, float(configCount) * 100 / float(numConfigs))
-        #print "C: %.4f    r: %.4f" % (C, r)
-        print "C: %.4f      r: %.2f" % (C, r)
-        configCount += 1
-        splitCount = 1
-        accuracies = []
-        for trainInds, evalInds in StratifiedKFold(n_splits=5, random_state=0).split(x_train, y_train):
-            x_train_split = x_train[trainInds]
-            y_train_split = y_train[trainInds]
-            x_eval_split = x_train[evalInds]
-            y_eval_split = y_train[evalInds]
-
-            model = SVM(C=C, r=r)
-            model.fit(x_train_split, y_train_split, epochs=numEpochs)
-            predictions = model.predict(x_eval_split)
-            accuracy = float(np.sum(np.where(predictions == y_eval_split, 1, 0))) / float(len(y_eval_split))
-            accuracies.append(accuracy)
-            print "Split %i of %i. Accuracy: %.2f" % (splitCount, 5, accuracy)
-
-            splitCount += 1
-        print ""
-        averageAccuracy = np.mean(np.array(accuracies))
-        if averageAccuracy > bestParams["accuracy"]:
-            bestParams["accuracy"] = averageAccuracy
-            bestParams["params"]["C"] = C
-            bestParams["params"]["r"] = r
-
-
-
-print "Best params for %s:" % type(model)
-print bestParams["params"]
-print "Best Average Training Accuracy:"
-print bestParams["accuracy"]
-
-bestModel = model.set_params(bestParams["params"])
-bestModel.fit(x_train, y_train)
-
-trainingPredictions = bestModel.predict(x_train)
-trainingAccuracy = np.sum(np.where(trainingPredictions == y_train, 1., 0.)) / float(len(y_train))
-print "Training Accuracy: %f" % trainingAccuracy
-
-predictions = bestModel.predict(x_test)
-
-accuracy = np.sum(np.where(predictions == y_test, 1., 0.)) / float(len(y_test))
-print "Test Accuracy: %f" % accuracy
-
-# # 2. Logistic Regression ********************************************************
-# print "\n\nLogistic Regression ********************************************************"
-#
-# tradeoff = [10., 100., 1000., 10000.]
-# rate = [.001, .0001, .000001]
-# numEpochs = 3
-# bestParams = {"params": {}, "accuracy": 0}
+# # Test SVM ******************************
+# svmC = [3000, 1000, 100]
+# rate = [.5, .1, .01, .001, .00001]
+# numEpochs = 40
+# bestParams = {"params": {}, "accuracy" : 0}
 # configCount = 1
-# numConfigs = len(tradeoff) * len(rate)
-# for tradeoffConstant in tradeoff:
+# numConfigs = len(svmC) * len(rate)
+# for C in svmC:
 #     for r in rate:
-#         print "Evaluating configuration %i of %i. (%.2f%%)" % (
-#         configCount, numConfigs, float(configCount) * 100 / float(numConfigs))
-#         print "Tradeoff: %.4f      r: %.2f" % (tradeoffConstant, r)
+#         print "Evaluating configuration %i of %i. (%.2f%%)" % (configCount, numConfigs, float(configCount) * 100 / float(numConfigs))
+#         #print "C: %.4f    r: %.4f" % (C, r)
+#         print "C: %.4f      r: %.2f" % (C, r)
 #         configCount += 1
 #         splitCount = 1
 #         accuracies = []
@@ -95,18 +41,22 @@ print "Test Accuracy: %f" % accuracy
 #             x_eval_split = x_train[evalInds]
 #             y_eval_split = y_train[evalInds]
 #
-#             model = LogisticRegression(sigma=tradeoffConstant, r=r)
-#             model.fit(x_train_split, y_train_split)
+#             model = SVM(C=C, r=r)
+#             model.fit(x_train_split, y_train_split, epochs=numEpochs)
 #             predictions = model.predict(x_eval_split)
 #             accuracy = float(np.sum(np.where(predictions == y_eval_split, 1, 0))) / float(len(y_eval_split))
 #             accuracies.append(accuracy)
 #             print "Split %i of %i. Accuracy: %.2f" % (splitCount, 5, accuracy)
+#
 #             splitCount += 1
+#         print ""
 #         averageAccuracy = np.mean(np.array(accuracies))
 #         if averageAccuracy > bestParams["accuracy"]:
 #             bestParams["accuracy"] = averageAccuracy
-#             bestParams["params"]["sigma"] = tradeoffConstant
+#             bestParams["params"]["C"] = C
 #             bestParams["params"]["r"] = r
+#
+#
 #
 # print "Best params for %s:" % type(model)
 # print bestParams["params"]
@@ -124,7 +74,69 @@ print "Test Accuracy: %f" % accuracy
 #
 # accuracy = np.sum(np.where(predictions == y_test, 1., 0.)) / float(len(y_test))
 # print "Test Accuracy: %f" % accuracy
-#
+
+# 2. Logistic Regression ********************************************************
+print "\n\nLogistic Regression ********************************************************"
+
+tradeoff = [10., 100., 1000., 10000.]
+rate = [.1, .01, .001]
+numEpochs = 3
+bestParams = {"params": {}, "accuracy": 0}
+configCount = 1
+numConfigs = len(tradeoff) * len(rate)
+for tradeoffConstant in tradeoff:
+    for r in rate:
+        print "Evaluating configuration %i of %i. (%.2f%%)" % (
+        configCount, numConfigs, float(configCount) * 100 / float(numConfigs))
+        print "Tradeoff: %.4f      r: %.2f" % (tradeoffConstant, r)
+        configCount += 1
+        splitCount = 1
+        accuracies = []
+        for trainInds, evalInds in StratifiedKFold(n_splits=5, random_state=0).split(x_train, y_train):
+            x_train_split = x_train[trainInds]
+            y_train_split = y_train[trainInds]
+            x_eval_split = x_train[evalInds]
+            y_eval_split = y_train[evalInds]
+
+            model = LogisticRegression(sigma=tradeoffConstant, r=r)
+            model.fit(x_train_split, y_train_split)
+            predictions = model.predict(x_eval_split)
+            accuracy = float(np.sum(np.where(predictions == y_eval_split, 1, 0))) / float(len(y_eval_split))
+            accuracies.append(accuracy)
+            print "Split %i of %i. Accuracy: %.2f" % (splitCount, 5, accuracy)
+            splitCount += 1
+        averageAccuracy = np.mean(np.array(accuracies))
+        if averageAccuracy > bestParams["accuracy"]:
+            bestParams["accuracy"] = averageAccuracy
+            bestParams["params"]["sigma"] = tradeoffConstant
+            bestParams["params"]["r"] = r
+
+print "Best params for %s:" % type(model)
+print bestParams["params"]
+print "Best Average Training Accuracy:"
+print bestParams["accuracy"]
+
+bestModel = model.set_params(bestParams["params"])
+bestModel.fit(x_train, y_train)
+
+rawTrainingPredictions = bestModel.predict(x_train)
+trainingPredictions = np.where(rawTrainingPredictions == 1., 1., 0.)
+trainingAccuracy = np.sum(np.where(trainingPredictions == y_train, 1., 0.)) / float(len(y_train))
+print "Training Accuracy: %f" % trainingAccuracy
+print "Training precision, recall, fscore:"
+print precision_recall_fscore_support(y_train, trainingPredictions, average="binary")
+
+predictions = np.where(bestModel.predict(x_test) == 1., 1., 0.)
+
+accuracy = np.sum(np.where(predictions == y_test, 1., 0.)) / float(len(y_test))
+print "Test Accuracy: %f" % accuracy
+
+print "Testing precision, recall, fscore:"
+print precision_recall_fscore_support(y_test, predictions, average="binary")
+
+accuracy = np.sum(np.where(predictions == y_test, 1., 0.)) / float(len(y_test))
+print "Test Accuracy: %f" % accuracy
+
 #
 # # 3. Naive Bayes ********************************************************
 # print "\n\nNaive Bayes ********************************************************"
@@ -180,14 +192,18 @@ print "Test Accuracy: %f" % accuracy
 # # 3. Bagged Forests ********************************************************
 # print "\n\nBagged Forests ********************************************************"
 #
-# model = BaggedForest(numTrees=1000, depth=10)
+# model = BaggedForest(numTrees=1000, depth=3)
 # model.fit(x_train, y_train)
 #
-# trainingPredictions = model.predict(x_train)
+# trainingPredictions = np.where(model.predict(x_train) == 1., 1., 0.)
 # trainingAccuracy = np.sum(np.where(trainingPredictions == y_train, 1., 0.)) / float(len(y_train))
 # print "Training Accuracy: %f" % trainingAccuracy
+# print "Training precision, recall, fscore:"
+# print precision_recall_fscore_support(y_train, trainingPredictions, average="binary")
 #
-# predictions = model.predict(x_test)
+# predictions = np.where(model.predict(x_test) == 1., 1., 0.)
+# print "Testing precision, recall, fscore:"
+# print precision_recall_fscore_support(y_test, predictions, average="binary")
 #
 # accuracy = np.sum(np.where(predictions == y_test, 1., 0.)) / float(len(y_test))
 # print "Test Accuracy: %f" % accuracy
@@ -308,6 +324,6 @@ print "Test Accuracy: %f" % accuracy
 # print "Training Accuracy: %f" % trainingAccuracy
 #
 # predictions = bestModel.predict(baggedForestPredictionsTest)
-# 
+#
 # accuracy = np.sum(np.where(predictions == y_test, 1., 0.)) / float(len(y_test))
 # print "Test Accuracy: %f" % accuracy
